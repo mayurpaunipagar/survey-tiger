c293bf926b10555ee7113d26bdad7e18033a9998
MayurHpNotebook <001mayur94@gmail.com>
2021-01-02 23:31:28 +0530

    added question buttons in single-select

--- src/components/single-select.js
+++ src/components/single-select.js
@@ -1,13 +1,33 @@
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
-
+import { Button } from 'reactstrap';
 function SingleSelect() {
   return (<div className="question-container">
-    <InputGroup>
+    <InputGroup className="input-grp">
       <InputGroupAddon addonType="prepend">
         <InputGroupText>@</InputGroupText>
       </InputGroupAddon>
       <Input placeholder="username" />
     </InputGroup>
+    <p className="options-text">Options</p>
+    <InputGroup className="input-grp">
+      <Input placeholder="Option 1" />
+      <InputGroupAddon addonType="append">
+        <InputGroupText>+</InputGroupText>
+        <InputGroupText>-</InputGroupText>
+      </InputGroupAddon>
+    </InputGroup>
+    <InputGroup className="input-grp">
+      <Input placeholder="Option 1" />
+      <InputGroupAddon addonType="append">
+        <InputGroupText>+</InputGroupText>
+        <InputGroupText>-</InputGroupText>
+      </InputGroupAddon>
+    </InputGroup>
+    <div className="question-buttons">
+      <Button className="main-btn">Add Question</Button>
+      <Button className="main-btn">Publish</Button>
+    </div>
+
   </div>);
 }
 export default SingleSelect;
