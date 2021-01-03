bdb609cd781e115f9e8247a6b78003b923747f8b
MayurHpNotebook <001mayur94@gmail.com>
2021-01-04 04:39:13 +0530

    addquestion and publish are disabled if empty

--- src/components/multi-select.js
+++ src/components/multi-select.js
@@ -6,13 +6,15 @@ import {
     Input
 } from 'reactstrap';
 import { useState } from 'react';
-import Option from './option';
 
 function MultiSelect() {
     const [options, setOptions] = useState([""]);
-    const addOption = () => {
+    const [question, setQuestion] = useState("");
+    const addOption = (optionIdx) => {
         if (options.length < 4) {
-            setOptions([...options, ""]);
+            const newOptionIdx = optionIdx + 1;
+            options.splice(newOptionIdx, 0, "");
+            setOptions([...options]);
         }
     }
     const removeOption = (optionIdx) => {
@@ -21,30 +23,48 @@ function MultiSelect() {
             setOptions([...options]);
         }
     }
+    const setOptionsInArray = (value, optionIdx) => {
+        options[optionIdx] = value;
+        setOptions([...options]);
+    }
+    const isAddQuestionDisabled = () => question.trim() === "" ||
+        options.find((opt) => opt.trim() === "") !== undefined;
 
     return <div className="question-container">
         <InputGroup className="input-grp">
             <InputGroupAddon addonType="prepend">
                 <InputGroupText>?</InputGroupText>
             </InputGroupAddon>
-            <Input placeholder="Your Question" />
+            <Input placeholder="Your Question"
+                onChange={(e) => { 
+                    setQuestion(e.target.value)
+                 }}
+                value={question} />
         </InputGroup>
+
         <p className="options-text">Options</p>
         {options.map((option, optionIdx) => {
             return <>
                 <InputGroup className="input-grp">
                     <Input placeholder={`Option ${optionIdx + 1}`}
-                        value={option} />
+                        value={option}
+                        onChange={(e) => { setOptionsInArray(e.target.value, optionIdx) }}
+                        key={optionIdx} />
                     <InputGroupAddon addonType="append">
-                        <Button onClick={addOption} disabled={options.length === 4}>+</Button>
+                        <Button onClick={() => addOption(optionIdx)} disabled={options.length === 4}>+</Button>
                         <Button onClick={() => removeOption(optionIdx)} disabled={options.length === 1}>-</Button>
                     </InputGroupAddon>
                 </InputGroup>
             </>;
         })}
         {options.length === 4 ? (<div className="question-buttons">
-            <Button className="main-btn">Add Question</Button>
-            <Button className="main-btn">Publish</Button>
+            <Button className="main-btn"
+                disabled={isAddQuestionDisabled()}
+            >
+                Add Question
+            </Button>
+            <Button className="main-btn"
+                disabled={isAddQuestionDisabled()}>Publish</Button>
         </div>) : null}
 
     </div>;
